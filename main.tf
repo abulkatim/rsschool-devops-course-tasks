28254a70e9be9b85e46c37b739794218a59da0b6
Aslan <abulkatim@gmail.com>
2025-06-13 00:21:51 +0500

    removed acl

--- main.tf
+++ main.tf
@@ -5,8 +5,3 @@ provider "aws" {
 resource "aws_s3_bucket" "task_1" {
   bucket_prefix = "rs"
 }
-
-resource "aws_s3_bucket_acl" "task_1_acl" {
-  bucket = aws_s3_bucket.task_1.id
-  acl    = "private"
-}
