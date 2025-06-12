a70fdea3a601c1905ec83caf250c34d4c40eeadd
Aslan <abulkatim@gmail.com>
2025-06-12 21:54:27 +0500

    Update main.tf

--- s3-bucket-creation/main.tf
+++ s3-bucket-creation/main.tf
@@ -1,7 +1,9 @@
 provider "aws" {
     region = "us-east-1"
 }
-resource "aws_s3_bucket" "task_1_bucket"{
+resource "aws_s3_bucket" "task_1_bucket" {
 bucket_prefix = "rs-asap"
 acl = "private"
+
 }
+
