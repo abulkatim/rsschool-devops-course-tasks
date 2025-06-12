62394244b9d807c64272eb4f1c4b0853b5594873
Aslan <abulkatim@gmail.com>
2025-06-13 00:19:55 +0500

    aws region added

--- main.tf
+++ main.tf
@@ -4,5 +4,9 @@ provider "aws" {
 
 resource "aws_s3_bucket" "task_1" {
   bucket_prefix = "rs"
-  acl           = "private"
+}
+
+resource "aws_s3_bucket_acl" "task_1_acl" {
+  bucket = aws_s3_bucket.task_1.id
+  acl    = "private"
 }
