42dd964f0e44aa948a0b9648ca1823fc6eef8842
Aslan <abulkatim@gmail.com>
2025-06-13 00:14:39 +0500

    updated main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 provider "aws" {
-    region = "eu-central-1"
-}
-resource "aws_s3_bucket" "task_1_bucket" {
-bucket_prefix = "rs-asap"
-acl = "private"
+  region = "eu-central-1"
 }
 
+resource "aws_s3_bucket" "task_1" {
+  bucket_prefix = "rs"
+  acl           = "private"
+}
